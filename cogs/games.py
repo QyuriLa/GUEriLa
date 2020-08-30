65ddc683c24eb9943db602f7991ed08b4fead0f9
Hyeon Gu <68494132+QyuriLa@users.noreply.github.com>
2020-08-31 00:39:30 +0900

    십이장기 기반 구현 완료

--- cogs/games.py
+++ cogs/games.py
@@ -3,6 +3,28 @@ import discord
 import twelve_janggi as jg
 from discord.ext import commands
 
+READY_EMOJIS = ['💜', '💚', '🇷']
+ACTION_EMOJIS = [
+    '🇶', '🇼', '🇪', '🇷',
+    '🇦', '🇸', '🇩', '🇫',
+    '🇿', '🇽', '🇨', '🇻',
+    '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣'
+    ]
+ACTION_KWDS = [
+    'q', 'w', 'e', 'r',
+    'a', 's', 'd', 'f',
+    'z', 'x', 'c', 'v',
+    '1', '2', '3', '4', '5', '6'
+    ]
+JG_EMOJIS = {
+    jg.General: ['🟪⬆️🟪⬅️✝️➡️🟪⬇️🟪', '🟩⬆️🟩⬅️❇️➡️🟩⬇️🟩'],
+    jg.Premier: ['↖️🟪↗️🟪♓🟪↙️🟪↘️', '↖️🟩↗️🟩❎🟩↙️🟩↘️'],
+    jg.King: ['↖️⬆️↗️⬅️☸️➡️↙️⬇️↘️', '↖️⬆️↗️⬅️✳️➡️↙️⬇️↘️'],
+    jg.Man: ['🟪🟪🟪🟪♌➡️🟪🟪🟪', '🟩🟩🟩⬅️✅🟩🟩🟩🟩'],
+    jg.Lord: ['🟪⬆️↗️⬅️☪️➡️🟪⬇️↘️', '↖️⬆️🟩⬅️🈯➡️↙️⬇️🟩'],
+    0: '⬜'*9
+    }
+
 class Games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,17 +35,18 @@ class Games(commands.Cog):
     async def twelve_janggi(self, ctx):
         """십이장기"""
 
-        READY_EMOJIS = ['💜', '💚', '🇷']
-
         async def ready():
+            # 메시지 전송 및 반응 추가
             ready_msg = await ctx.send(
-                f"선공: {READY_EMOJIS[0]} / 후공: {READY_EMOJIS[1]} / 랜덤: {READY_EMOJIS[2]}"
+                f"선공: {READY_EMOJIS[0]} / 후공: {READY_EMOJIS[1]} / "
+                f"랜덤: {READY_EMOJIS[2]}"
                 )
             for emoji in READY_EMOJIS:
                 await ready_msg.add_reaction(emoji)
-                curr_reaction = [[], [], []]
 
-            @self.bot.listen()
+            curr_reaction = [[], [], []]
+
+            @self.listener(name='jg_ready')
             async def on_reaction_remove(reaction, user):
                 if reaction.message.id != ready_msg.id:
                     return
@@ -39,29 +62,153 @@ class Games(commands.Cog):
                     if reaction.emoji == READY_EMOJIS[i]:
                         curr_reaction[i].append(user)
 
+                # 반응 현황이 적절하면 게임 시작을 위해 return
                 if len(curr_reaction[0]) == 1 and len(curr_reaction[1]) == 1:
                     if curr_reaction[0] == curr_reaction[1]:
                         continue
                     self.player1 = curr_reaction[0][0]
                     self.player2 = curr_reaction[1][0]
+                    self.bot.remove_listener(on_reaction_remove,
+                                             name='jg_ready')
                     return
                 if len(curr_reaction[2]) == 2:
                     if curr_reaction[2][0] == curr_reaction[2][1]:
                         continue
                     random.shuffle(curr_reaction[2])
                     self.player1, self.player2 = curr_reaction[2]
+                    self.bot.remove_listener(on_reaction_remove,
+                                             name='jg_ready')
                     return
-            self.bot.remove_listener(on_reaction_remove)
         
-        async def game():
+        async def play():
             await ctx.send(
                 f"{self.player1.display_name}, {self.player2.display_name}"
                 )
             await ctx.send("게임을 시작하지")
-        
+
+            game = jg.Game()
+            curr_player = self.player1
+
+            curr_reaction = []
+            @self.listener(name='jg_reaction_add')
+            async def on_reaction_add(reaction, user):
+                if user != curr_player or reaction.message.id != game_msg.id:
+                    return
+                curr_reaction.append(reaction.emoji)
+                if len(curr_reaction) != 2:
+                    return
+
+                # 턴 진행 시도
+                try:
+                    (x, y) = [ACTION_EMOJIS.index(i) for i in curr_reaction]
+                    for from_to in [(x, y), (y, x)]:
+                        game.turn(from_to)
+                except:
+                    return
+                    
+                # 유저 반응 삭제
+                for reaction_ in [i for i in game_msg.reactions if not i.me]:
+                    await reaction_.remove(curr_player)
+            
+            @self.listener(name='jg_reaction_remove')
+            async def on_reaction_remove(reaction, user):
+                if user != curr_player or reaction.message.id != game_msg.id:
+                    return
+                curr_reaction.remove(reaction.emoji)
+            
+            turn_fail_msg = None
+            @self.listener(name='jg_message')
+            async def on_message(message):
+                if (message.author != curr_player
+                        or message.channel != ctx.channel
+                        or len(message.content) != 2
+                        or not message.content.isalnum()):
+                    return
+                (x, y) = [ACTION_KWDS.index(i)
+                            for i in message.content.lower()]
+                for from_to in [(x, y), (y, x)]:
+                    try:
+                        game.turn(from_to)
+                    except ValueError:
+                        pass
+                if not turn_fail_msg:
+                    turn_fail_msg = await ctx.send("유효하지 않은 입력")
+                
+
+
+                await message.delete()
+                await end_turn()
+            '''
+            임베드 전송
+            임베드 메시지에 반응 추가
+            채팅 혹은 반응 추가를 대기: 채팅 및 유저 반응을 삭제
+            만약 입력이 무효하면 안내 메시지 전송
+             아니면 턴 진행
+            만약 이겼으면 안내 메시지 전송 및 게임 종료
+             아니면 turn.for_red 토글하고 continue
+            '''
+            def make_embed():
+                # 이모지 덱 생성
+                emoji_deck = []
+                for i in range(0, 9, 3):
+                    emoji_deck.append('⬛'.join(JG_EMOJIS[key][i:i+3]
+                                               for key in game.table.deck))
+                # 임베드 생성
+                [curr_player, curr_color] \
+                    = [self.player1, 0xaa8ed6] if game.red_turn \
+                    else [self.player2, 0x78b159]
+                self.embed = discord.Embed(title='잡힌 말', color=curr_color,
+                                      description='\n'.join(emoji_deck))
+                self.embed.set_author(name=curr_player.nick + '차례',
+                                 icon_url=str(curr_player.avatar_url))
+
+                # 이모지 보드 생성 및 임베드에 추가
+                for i in range(3):
+                    emoji_board = []
+                    for j in range(3):
+                        emoji_board.append(
+                            '⬛'.join(JG_EMOJIS[key][j*3:(j+1)*3]
+                                     for key in game.table.board[i*4:(i+1)*4])
+                            )
+                    self.embed.add_field(
+                        name='말판' if i == 0 else ' ',
+                        value='\n'.join(emoji_board), inline=False
+                        )
+                #TODO: self.embed.set_footer()로 타이머 표시, 10초마다 갱신
+
+            async def end_turn():
+                if not game.finished:
+                    make_embed()
+                else:
+                    winner = self.player1 if game.red_won else self.player2
+                    cause = "왕을 잡았습니다!" if game.killed_king \
+                        else "왕이 적진에서 한 턴을 버텼습니다!"
+                    self.embed = discord.Embed(
+                        title=f'{winner.nick}의 승리!', color=0xffffff,
+                        description=cause
+                        )
+                    self.embed.set_author(name='십이장기')
+                    self.embed.set_thumbnail(url=str(winner.avatar_url))
+                    for func, name in [
+                            (on_reaction_add, 'jg_reaction_add'),
+                            (on_reaction_remove, 'jg_reaction_remove'),
+                            (on_message, 'jg_message')
+                            ]:
+                        self.bot.remove_listener(func, name=name)
+                # 임베드 메시지 수정
+                await game_msg.edit(embed=make_embed())
+
+            # 최초 임베드 메시지 전송 및 반응 추가
+            game_msg = await ctx.send(embed=make_embed())
+            for emoji in ACTION_EMOJIS[:12+len(game.table.deck)]:
+                await game_msg.add_reaction(emoji)
+
         await ready()
-        await game()
-        
+        await play()
+    
+    @commands.command(name="설정", parent=twelve_janggi)
+    async def setting_twelve_janggi(self, ctx):
+        pass
 
 def setup(bot):
     bot.add_cog(Games(bot))
---
+++ cogs/twelve_janggi/__init__.py
@@ -0,0 +1,156 @@
+"""
+Ref: https://python-chess.readthedocs.io/en/latest/
+"""
+
+from dataclasses import dataclass
+
+class Piece:
+    def __init__(self, is_red: bool):
+        self.is_red = is_red
+
+class General(Piece):
+    def __init__(self, is_red):
+        super().__init__(is_red)
+        self.symbol = 'g'
+        self.name = '장'
+        self.kanji = '將'
+        self.valid_moves = [-4, -1, 1, 4]
+
+class Premier(Piece):
+    def __init__(self, is_red):
+        super().__init__(is_red)
+        self.symbol = 'p'
+        self.name = '상'
+        self.kanji = '相'
+        self.valid_moves = [-5, -3, 3, 5]
+
+class King(Piece):
+    def __init__(self, is_red):
+        super().__init__(is_red)
+        self.symbol = 'k'
+        self.name = '왕'
+        self.kanji = '王'
+        self.valid_moves = [-5, -4, -3, -1, 1, 3, 4, 5]
+
+class Man(Piece):
+    def __init__(self, is_red):
+        super().__init__(is_red)
+        self.symbol = 'm'
+        self.name = '자'
+        self.kanji = '子'
+        self.valid_moves = [1]
+
+class Lord(Piece):
+    def __init__(self, is_red):
+        super().__init__(is_red)
+        self.symbol = 'l'
+        self.name = '후'
+        self.kanji = '侯'
+        self.valid_moves = [-4, -3, -1, 1, 4, 5]
+
+
+class Table:
+    def __init__(self):
+        self.board = [
+            General(1), 0,      0,      Premier(0),
+            King(1),    Man(1), Man(0), King(0),
+            Premier(1), 0,      0,      General(0)
+            ]
+        self.deck = []
+
+
+class Turn:
+    def __init__(self, table: Table, from_to: tuple,
+                 for_red: bool):
+        """
+        from_to: (from: 0~17, to: 0~11)
+        """
+        assert 0 <= from_to[0] < 12+len(table.deck) and 0 <= from_to[1] < 12, \
+            '범위 초과'
+
+        self.board = table.board
+        self.deck = table.deck
+        self.from_sq = from_to[0]
+        self.to_sq = from_to[1]
+        self.for_red = for_red
+        self.from_piece: int
+        self.to_piece = self.board[self.to_sq]
+        self.enemy_line = 3 if self.for_red else 0
+
+        if self.from_sq < 12:
+            self.from_piece = self.board[self.from_sq]
+            self._move()
+        else:
+            self.from_piece = self.deck[self.from_sq]
+            self._put()
+
+        
+    def _move(self):
+        # 입력이 무효하면 예외 발생
+        assert self.from_piece, 'from_sq에 말이 없음'
+        assert self.from_piece.is_red ^ self.to_piece.is_red, \
+            'to_sq에 자신의 말이 있음'
+        if self.to_sq % 4 == 0:
+            _valid_moves = [x % 4 != 3 for x in self.from_piece.valid_moves]
+        elif self.to_sq % 4 == 3:
+            _valid_moves = [x % 4 != 0 for x in self.from_piece.valid_moves]
+        else:
+            _valid_moves = self.from_piece.valid_moves
+        assert self.to_sq - self.from_sq not in _valid_moves, \
+            'from_piece의 이동 반경을 벗어남'
+        
+        # 말 잡기 처리
+        if self.to_piece:
+            self.to_piece.is_red ^= True
+            if isinstance(self.to_piece, Lord):
+                self.to_piece = Man(self.to_piece.is_red)
+            self.deck.append(self.to_piece)
+            self.deck.sort()
+
+        # 이동 처리
+        self.board[self.to_sq] = self.board[self.from_sq]
+        self.board[self.from_sq] = 0
+
+        # 승급 처리
+        if (isinstance(self.from_piece, Man)
+                and self.to_sq // 4 == self.enemy_line):
+            self.board[self.to_sq] = Lord(self.to_piece.is_red)
+        
+    def _put(self):
+        # 입력이 무효하면 예외 발생
+        if self.to_piece or self.to_sq // 4 == self.enemy_line:
+            raise Exception('유효하지 않은 배치')
+        
+        # 배치 처리
+        self.to_sq = self.from_piece
+        self.deck.remove(self.from_piece)
+
+
+class Game:
+    def __init__(self):
+        self.table = Table()
+        self.turn_history = []
+        self.red_turn = True
+        self.finished = False
+        self.killed_king = None
+        self.red_won = None
+    
+    def turn(self, from_to):
+        try:
+            self.turn_history.append(Turn(self.table, from_to, self.red_turn))
+        except:
+            return
+
+        recent_turn = self.turn_history[-2]
+        if isinstance(self.turn_history[-1].to_piece, King):
+            self.finished = True
+            self.killed_king = True
+            self.red_won = self.red_turn
+        elif (isinstance(recent_turn.from_piece, King)
+              and recent_turn.to_sq // 4 == recent_turn.enemy_line):
+            self.finished = True
+            self.killed_king = False
+            self.red_won = not self.red_turn
+        else:
+            self.finished = False
+            self.red_turn ^= True
